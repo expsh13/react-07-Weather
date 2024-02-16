91c265ee2086716485b392399116097de5e93eed
Satoki Hata <satokihata@gmail.com>
2024-02-16 12:52:41 +0900

    add data

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,16 @@
 export const Card = () => {
   return (
-    <div className="p-1 bg-green-100 rounded text-center">
-      <p className="font-bold">Card</p>
+    <div className="p-1 bg-green-100 rounded text-center grid gap-2">
+      <p className="font-bold">日付</p>
       <img
-        src="https://dummyimage.com/10x30/fff/fff.png?text=%E3%83%80%E3%83%9F%E3%83%BC"
+        src="https://dummyimage.com/200x50/fff/fff.png?text=%E3%83%80%E3%83%9F%E3%83%BC"
         alt=""
       />
+      <p>天気：</p>
+      <ul>
+        <li>最高気温：</li>
+        <li>最低気温：</li>
+      </ul>
     </div>
   );
 };
